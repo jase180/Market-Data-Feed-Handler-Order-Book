11a14dec8ff022f022c4991e5bdf096dd66bdd5c
jason <jase180@gmail.com>
2025-10-03 12:57:45 -0500

    Modified main.cpp to test OrderBook and working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "OrderBook.h"
 
+// Test OrderBook works
 int main() {
-    std::cout << "Market Data Feed Handler\n";
+    // Create an order book for Apple stock
+    OrderBook appleBook("AAPL");
+    
+    // Add some buy orders (bids)
+    appleBook.addBid(150.25, 100);
+    appleBook.addBid(150.24, 200);
+    appleBook.addBid(150.20, 150);
+    
+    // Add some sell orders (asks)
+    appleBook.addAsk(150.27, 100);
+    appleBook.addAsk(150.28, 150);
+    appleBook.addAsk(150.30, 200);
+
+    // Print the order book
+    appleBook.print();
+    
     return 0;
-};
+}
